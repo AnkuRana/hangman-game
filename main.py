98b8ac9909ee58aa1875dc59d27794b58d591544
AnkuRana <amitrana.com0007@gmail.com>
2022-10-05 11:11:57 +0000

    Improved code readability and UX.

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-#Step 4
-
 import random
-import hangman_art
-import hangman_words
-
-print(hangman_art.logo)
+from hangman_words import word_list
+from hangman_art import logo ,stages
+print(logo)
 end_of_game = False
-chosen_word = random.choice(hangman_words.word_list)
+chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
-#TODO-1: - Create a variable called 'lives' to keep track of the number of lives left.
-#Set 'lives' to equal 7.
+#Set 'lives' to equal 6.
 lives = 6
 
 #Testing code
@@ -21,7 +17,7 @@ user_entered_letters = []
 for _ in range(word_length):
     display += "_"
 
-while lives >= 0:
+while lives > 0:
     guess = input("Guess a letter: ").lower()
     
     if guess in user_entered_letters:
@@ -36,9 +32,6 @@ while lives >= 0:
         if letter == guess:
             display[position] = letter
             element_check = 1
-#TODO-2: - If guess is not a letter in the chosen_word,
-#Then reduce 'lives' by 1.
-#If lives goes down to 0 then the game should stop and it should print "You lose."
 
     #Join all the elements in the list and turn it into a String.
     print(f"{' '.join(display)}")
@@ -47,13 +40,12 @@ while lives >= 0:
     if "_" not in display:
         end_of_game = True
         break
-#TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
+#print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
     if element_check == 0:
-        print(hangman_art.stages[lives])
         print(f"You have guessed wrong letter {guess}, you loose a life - Guess again")
         lives -= 1
-        
-        print(f"you have {lives+1} left.")
+        print(f"you have {lives} left.")
+    print(stages[lives])
 
 if end_of_game == True:
     print("You Win")
